a1de7971570b1d2b234d6fdcd2e5a27e05f5ae5f
Мик <mishlmish@inbox.ru>
2024-05-26 19:57:24 +0300

    3. Итератор четных чисел [#150]

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,42 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean rsl = false;
+        for (int i = index; i < data.length; i++) {
+            if (data[i] % 2 == 0) {
+                index = i;
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+
+    public static void main(String[] args) {
+        int[] data = {27, 8, 9, -3, -44};
+        EvenNumbersIterator iterator = new EvenNumbersIterator(data);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
