93edd4bddb97b8617787496a10c93818bb9447d0
Мик <mishlmish@inbox.ru>
2024-06-03 02:11:36 +0300

    deletion

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -10,43 +10,6 @@ public class ForwardLinked<T> implements Iterable<T> {
     private int modCount;
     private ForwardLinked.Node<T> head;
 
-    public void add(T value) {
-        if (head == null) {
-            head = new Node<>(value, null);
-        } else {
-            Node<T> tail = head;
-            for (int i = 0; i < size - 1; i++) {
-                tail = tail.next;
-            }
-            tail.next = new Node<T>(value, null);
-        }
-        size++;
-        modCount++;
-    }
-
-    public T get(int index) {
-        Objects.checkIndex(index, size);
-        Node<T> indexNode = head;
-        for (int i = 0; i < index; i++) {
-            indexNode = indexNode.next;
-        }
-        return indexNode.value;
-    }
-
-    public T deleteFirst() {
-        if (head == null) {
-            throw new NoSuchElementException();
-        }
-        T value = head.value;
-        Node<T> next = head.next;
-        head.next = null;
-        head.value = null;
-        head = next;
-        size--;
-        modCount++;
-        return value;
-    }
-
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
