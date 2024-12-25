4df089c25f6c5dc1fb9dae5c8b546a7d78dede9e
Мик <mishlmish@inbox.ru>
2024-12-26 01:38:47 +0300

    10. Сканирование файловой системы [#106929] v2

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -4,36 +4,35 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
-public class SearchFiles {
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    private final List<Path> paths = new ArrayList<>();
+    private final Predicate<Path> condition;
 
-    public  static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
-        List<Path> paths = new ArrayList<>();
-        Files.walkFileTree(root, new SimpleFileVisitor<>() {
-
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (condition.test(file)) {
-                    paths.add(file);
-                }
-                return CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                System.err.println("fail to visit file " + file + " - " + exc.getMessage());
-                return CONTINUE;
-            }
-        });
-        return paths;
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
     }
 
-    public static void main(String[] args) throws IOException {
-        search(Paths.get("."), path -> path.toString().endsWith(".js"))
-                .forEach(System.out::println);
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            paths.add(file);
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.err.println("Fail to visit file " + file + " - " + exc.getMessage());
+        return CONTINUE;
+    }
+
+    public List<Path> getPaths() {
+        return Collections.unmodifiableList(paths);
     }
 }
